a21b72914719272c8523a678b7035946d47b451e
Stine <stinev@uia.no>
2025-03-14 13:41:10 +0100

    fikset: konfigurasjonen

--- express-post-sum/db.js
+++ express-post-sum/db.js
@@ -1,14 +1,9 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "familietre_db_user",
-  host: "dpg-cv81ld8gph6c7399b14g-a.oregon-postgres.render.com",
-  database: "familietre_db",
-  password: "5t05c45sou3oKlrT6yF4fq1Mse6e4BXD",
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false, // Legger til SSL-støtte
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
