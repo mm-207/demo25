8f1bfb7fa7c7254831ea2335765d16083a17a57d
Christian Simonsen <crismo@users.noreply.github.com>
2025-04-14 15:46:17 +0200

    Update server.mjs
    removing all server functionality except static.

--- server.mjs
+++ server.mjs
@@ -1,30 +1,30 @@
 import express from 'express'
-import HTTP_CODES from './utils/httpCodes.mjs';
-import log from './modules/log.mjs';
-import { LOGG_LEVELS, eventLogger } from './modules/log.mjs';
-import { startSession, updateSession } from './modules/session.mjs';
-import treeRouter from './routes/treeAPI.mjs';
-import questLogRouter from './routes/questLogAPI.mjs';
-import userRouter from './routes/userAPI.mjs';
+//import HTTP_CODES from './utils/httpCodes.mjs';
+//import log from './modules/log.mjs';
+//import { LOGG_LEVELS, eventLogger } from './modules/log.mjs';
+//import { startSession, updateSession } from './modules/session.mjs';
+//import treeRouter from './routes/treeAPI.mjs';
+//import questLogRouter from './routes/questLogAPI.mjs';
+//import userRouter from './routes/userAPI.mjs';
 
 const ENABLE_LOGGING = false;
 
 const server = express();
 const port = (process.env.PORT || 8000);
 
-const logger = log(LOGG_LEVELS.VERBOSE);
+//const logger = log(LOGG_LEVELS.VERBOSE);
 
 server.set('port', port);
-server.use(logger);
-server.use(startSession);
+//server.use(logger);
+//server.use(startSession);
 server.use(express.static('public'));
-server.use("/tree/", treeRouter);
-server.use("/quest", questLogRouter);
-server.use("/user", userRouter)
+//server.use("/tree/", treeRouter);
+//server.use("/quest", questLogRouter);
+//server.use("/user", userRouter)
 
 
 
-server.use(updateSession);
+//server.use(updateSession);
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
