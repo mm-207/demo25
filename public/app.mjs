a5aa7bdb3c2903d4ff45379702cc74a6ef04d05e
Christian Simonsen <crismo@users.noreply.github.com>
2025-04-14 15:43:37 +0200

    Update app.mjs
    revertet til sw simple.

--- public/app.mjs
+++ public/app.mjs
@@ -1,11 +1,6 @@
-import LoginViewController from "./controller/loginView.mjs"
-
-console.log("????")
 
 if ("serviceWorker" in navigator) {
+    console.log("Hey I am installing service worker");
     navigator.serviceWorker.register("/sw.js");
+    console.log("Done");
 }
-
-console.log(LoginViewController);
-
-document.body.append(LoginViewController.view);
